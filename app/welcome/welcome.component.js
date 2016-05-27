41ff5ac8f1c9c2eaad191c33a4bdbbbc59005aed
Randy Lee <Randy Lee>
2016-05-27 17:35:56 -0400

    Main Menu fixed with transitions

--- app/welcome/welcome.component.js
+++ app/welcome/welcome.component.js
@@ -1,4 +1,4 @@
-System.register(['@angular/core', '../shared/service/core-services.service', '../shared/hub/hub.service', 'ng2-bootstrap'], function(exports_1, context_1) {
+System.register(['@angular/core', '../shared/service/core-services.service', '../shared/hub/hub.service', 'ng2-bootstrap', 'lodash'], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
@@ -10,7 +10,7 @@ System.register(['@angular/core', '../shared/service/core-services.service', '..
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1, core_services_service_1, hub_service_1, ng2_bootstrap_1;
+    var core_1, core_services_service_1, hub_service_1, ng2_bootstrap_1, lodash_1;
     var WelcomeComponent;
     return {
         setters:[
@@ -25,6 +25,9 @@ System.register(['@angular/core', '../shared/service/core-services.service', '..
             },
             function (ng2_bootstrap_1_1) {
                 ng2_bootstrap_1 = ng2_bootstrap_1_1;
+            },
+            function (lodash_1_1) {
+                lodash_1 = lodash_1_1;
             }],
         execute: function() {
             WelcomeComponent = (function () {
@@ -32,29 +35,94 @@ System.register(['@angular/core', '../shared/service/core-services.service', '..
                     var _this = this;
                     this.xCoreServices = xCoreServices;
                     this.hubService = hubService;
-                    this.oneAtATime = true;
-                    this.items = ['Item 1', 'Item 2', 'Item 3'];
-                    this.status = {
-                        isFirstOpen: true,
-                        isFirstDisabled: false
-                    };
-                    this.groups = [
-                        {
-                            title: 'Dynamic Group Header - 1',
-                            content: 'Dynamic Group Body - 1'
-                        },
-                        {
-                            title: 'Dynamic Group Header - 2',
-                            content: 'Dynamic Group Body - 2'
-                        }
-                    ];
+                    this.hubData = { ApiEndpoints: [], MenuItems: [], Scopes: "" };
+                    this.menuItems = [];
+                    this.menuItemIdGenerator = 0;
                     //Set up events
                     this.hubService.HubDataCompletedEvent.subscribe(function (hd) {
                         _this.hubData = hd;
+                        _this.hubData.MenuItems = lodash_1.default.chain(_this.hubData.MenuItems)
+                            .sortBy(function (mi) { return mi.Description; })
+                            .value();
+                        _this.menuItems = _this.flattenMenuItems();
                     });
                 }
-                WelcomeComponent.prototype.addItem = function () {
-                    this.items.push("Items " + (this.items.length + 1));
+                ;
+                WelcomeComponent.prototype.visibleMenuItems = function () {
+                    return lodash_1.default.chain(this.menuItems).filter(function (mi) { return mi.IsDisplayed; }).value();
+                };
+                WelcomeComponent.prototype.navigateToRoute = function (route) {
+                    if (!route)
+                        return;
+                    this.xCoreServices.Router.navigate([route]);
+                };
+                WelcomeComponent.prototype.flattenMenuItems = function () {
+                    var ret = [];
+                    var level = 1;
+                    var parents = [];
+                    this.menuItemIdGenerator = 0;
+                    this.getMenuItemChildren(level, null, parents, this.hubData.MenuItems, ret);
+                    return ret;
+                };
+                WelcomeComponent.prototype.reactToItemClick = function (id) {
+                    var item = lodash_1.default.find(this.menuItems, function (mi) { return mi.Id == id; });
+                    if (!item)
+                        this.xCoreServices.LoggingService.warn("Unable to select element with Id " + id);
+                    if (item.MenuItem.SubMenus.length > 0) {
+                        this.setMenuItemState(item, !item.IsOpen);
+                    }
+                    else {
+                        this.navigateToRoute(item.MenuItem.Route);
+                    }
+                };
+                WelcomeComponent.prototype.setMenuItemState = function (item, open) {
+                    var _this = this;
+                    item.IsOpen = open;
+                    lodash_1.default.each(this.menuItems, function (mi) {
+                        //If an item has this item in the parent chain then check to see if its immediate parent is open
+                        //If it is then display it.  If not then don't
+                        if (lodash_1.default.findIndex(mi.Parents, function (p) { return p == item.Id; }) > -1) {
+                            if (mi.Parent && mi.Parent.IsOpen)
+                                mi.IsDisplayed = true;
+                            if (mi.Parent && !mi.Parent.IsOpen)
+                                mi.IsDisplayed = false;
+                            if (!open)
+                                mi.IsDisplayed = false;
+                        }
+                    });
+                    if (open) {
+                        //Set other items at this level to closed if they are open
+                        lodash_1.default.chain(this.menuItems)
+                            .filter(function (mi) { return mi.Id !== item.Id && mi.Level == item.Level; })
+                            .each(function (mi) { if (mi.IsOpen)
+                            _this.setMenuItemState(mi, false); }).value();
+                    }
+                };
+                WelcomeComponent.prototype.hasSubItems = function (menuItem) {
+                    return menuItem.MenuItem.SubMenus.length > 0;
+                };
+                WelcomeComponent.prototype.showDownCaret = function (menuItem) {
+                    return menuItem.MenuItem.SubMenus.length > 0 && menuItem.IsOpen;
+                };
+                WelcomeComponent.prototype.showLeftCaret = function (menuItem) {
+                    return menuItem.MenuItem.SubMenus.length > 0 && !menuItem.IsOpen;
+                };
+                WelcomeComponent.prototype.getMenuItemChildren = function (level, parent, parents, currentChildren, allItems) {
+                    var _this = this;
+                    lodash_1.default.each(currentChildren, function (mi) {
+                        _this.menuItemIdGenerator += 1;
+                        var newMenuItem = {
+                            Id: _this.menuItemIdGenerator,
+                            MenuItem: mi,
+                            IsDisplayed: (level == 1),
+                            Level: level,
+                            Parents: [].concat(parents),
+                            Parent: parent,
+                            IsOpen: false
+                        };
+                        allItems.push(newMenuItem);
+                        _this.getMenuItemChildren(level + 1, newMenuItem, parents.concat(newMenuItem.Id), mi.SubMenus, allItems);
+                    });
                 };
                 WelcomeComponent.prototype.ngOnInit = function () {
                 };
@@ -62,7 +130,8 @@ System.register(['@angular/core', '../shared/service/core-services.service', '..
                     core_1.Component({
                         templateUrl: 'app/welcome/welcome.component.html',
                         directives: [ng2_bootstrap_1.ACCORDION_DIRECTIVES],
-                        providers: []
+                        providers: [],
+                        styleUrls: ['app/welcome/welcome.component.css']
                     }), 
                     __metadata('design:paramtypes', [core_services_service_1.XCoreServices, hub_service_1.HubService])
                 ], WelcomeComponent);
--- app/welcome/welcome.component.ts
+++ app/welcome/welcome.component.ts
@@ -1,49 +1,122 @@
 import { Component, OnInit } from '@angular/core';
 import { XCoreServices } from '../shared/service/core-services.service';
-import { HubService, IHubServiceData } from '../shared/hub/hub.service';
+import { HubService, IHubServiceData, IHubServiceMenuItem } from '../shared/hub/hub.service';
 import { ACCORDION_DIRECTIVES } from 'ng2-bootstrap';
+
+import _ from 'lodash';
 
 @Component({
     templateUrl: 'app/welcome/welcome.component.html',
     directives: [ACCORDION_DIRECTIVES],
-    providers: []
+    providers: [],
+    styleUrls: ['app/welcome/welcome.component.css']
 })
 export class WelcomeComponent implements OnInit {
 
-    public hubData: IHubServiceData;
-
-
-    public oneAtATime: boolean = true;
-    public items: Array<string> = ['Item 1', 'Item 2', 'Item 3'];
-
-    public status: Object = {
-        isFirstOpen: true,
-        isFirstDisabled: false
-    };
-
-    public groups: Array<any> = [
-        {
-            title: 'Dynamic Group Header - 1',
-            content: 'Dynamic Group Body - 1'
-        },
-        {
-            title: 'Dynamic Group Header - 2',
-            content: 'Dynamic Group Body - 2'
-        }
-    ];
-
-    public addItem(): void {
-        this.items.push(`Items ${this.items.length + 1}`);
-    }
-
+    public hubData: IHubServiceData =  { ApiEndpoints: [], MenuItems: [], Scopes:"" };;
+    public menuItems: IMainMenuItem[] = [];
+    public menuItemIdGenerator: number = 0;
+    
     constructor(private xCoreServices: XCoreServices, private hubService: HubService) {
         //Set up events
         this.hubService.HubDataCompletedEvent.subscribe(hd => {
-            this.hubData = hd;            
+            this.hubData = hd;
+            this.hubData.MenuItems = _.chain(this.hubData.MenuItems)
+                .sortBy(mi => mi.Description)
+                .value();
+            this.menuItems = this.flattenMenuItems();
+            
         });
     }
 
+    public visibleMenuItems() {
+        return _.chain(this.menuItems).filter(mi => mi.IsDisplayed).value();
+    }
+    
+    public navigateToRoute(route: string): void {
+        if (!route) return;
+        this.xCoreServices.Router.navigate([route]);        
+    }
+
+    private flattenMenuItems(): IMainMenuItem[]  {
+        var ret: IMainMenuItem[] = [];
+        var level: number = 1;
+        var parents: number[] = [];
+        this.menuItemIdGenerator = 0;
+        this.getMenuItemChildren(level, null, parents, this.hubData.MenuItems, ret);
+        return ret;
+    }
+
+    public reactToItemClick(id: number): void {
+        var item = _.find(this.menuItems, mi => mi.Id == id);
+        if (!item) this.xCoreServices.LoggingService.warn(`Unable to select element with Id ${id}`);
+        if (item.MenuItem.SubMenus.length > 0) {
+            this.setMenuItemState(item, !item.IsOpen);
+        } else {
+            this.navigateToRoute(item.MenuItem.Route);
+        }
+    }
+    
+    private setMenuItemState(item: IMainMenuItem, open: boolean) {
+        item.IsOpen = open;        
+        _.each(this.menuItems, mi => {
+           //If an item has this item in the parent chain then check to see if its immediate parent is open
+           //If it is then display it.  If not then don't
+           if (_.findIndex(mi.Parents, p => p == item.Id) > -1) {
+                if (mi.Parent && mi.Parent.IsOpen) mi.IsDisplayed = true;
+                if (mi.Parent && !mi.Parent.IsOpen) mi.IsDisplayed = false;             
+                if (!open) mi.IsDisplayed = false;      
+           }                       
+        });
+        if (open) {
+            //Set other items at this level to closed if they are open
+            _.chain(this.menuItems)
+                .filter(mi => mi.Id !== item.Id && mi.Level == item.Level)
+                .each(mi => { if (mi.IsOpen) this.setMenuItemState(mi, false); }).value();            
+        }
+    }
+    
+    public hasSubItems(menuItem: IMainMenuItem): boolean {
+        return menuItem.MenuItem.SubMenus.length > 0    
+    }
+    
+    public showDownCaret(menuItem: IMainMenuItem): boolean {
+        return menuItem.MenuItem.SubMenus.length > 0 && menuItem.IsOpen;
+    }
+
+    public showLeftCaret(menuItem: IMainMenuItem): boolean {
+        return menuItem.MenuItem.SubMenus.length > 0 && !menuItem.IsOpen;
+    }
+    
+    private getMenuItemChildren(level: number, parent: IMainMenuItem, parents: number[], currentChildren: IHubServiceMenuItem[], allItems: IMainMenuItem[] ): void {
+        _.each(currentChildren, (mi) => {
+            this.menuItemIdGenerator += 1;
+            var newMenuItem: IMainMenuItem = {
+                Id: this.menuItemIdGenerator,
+                MenuItem: mi,
+                IsDisplayed: (level == 1),
+                Level: level,
+                Parents: [].concat(parents),
+                Parent: parent,
+                IsOpen: false
+            }
+            
+            allItems.push(newMenuItem);            
+            this.getMenuItemChildren(level + 1, newMenuItem, parents.concat(newMenuItem.Id), mi.SubMenus, allItems);
+        });    
+    }
+    
     ngOnInit() {
     }
 
+}
+
+export interface IMainMenuItem {
+    Id: number,
+    MenuItem: IHubServiceMenuItem,
+    IsDisplayed : boolean,
+    IsOpen: boolean,
+    Level: number,
+    Parents: number[],
+    Parent: IMainMenuItem
 }
